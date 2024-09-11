0ecf830581e4cf3fe2832f9271efdfb5591feaea
yashodha-25 <yashodagunawardhana15@gmail.com>
2024-09-11 11:13:12 +0530

    set dbconnection

---
+++ src/customer/db/DBConnection.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package customer.db;
+    import java.sql.Connection;
+    import java.sql.DriverManager;
+    import java.sql.SQLException;
+
+/**
+ *
+ * @author Yashoda
+ */
+public class DBConnection {
+    private static DBConnection dBConnection;
+    private Connection  connection;
+    
+    private DBConnection()throws SQLException, ClassNotFoundException{
+        Class.forName("com.mysql.cj.jdbc.Driver");{
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Supermarket", "root", "yash25");
+    }
+        
+    }
+    
+    public static DBConnection getInstance()throws SQLException, ClassNotFoundException{
+        if(dBConnection == null){
+           dBConnection = new DBConnection(); 
+        }
+        return dBConnection;
+    }
+    public Connection getConnection(){
+        return connection;
+    }
+    
+}
